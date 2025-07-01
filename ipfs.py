bb58575452773e113b2bea697240b30d196f6e72
Kaiwen Lin <kal283@pitt.edu>
2025-07-01 00:52:40 +0000

    Add ipfs.py for assignment

---
+++ ipfs.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+PINATA_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxZjY4MTRlNi0xYzM3LTQxZmMtOGFiOS1iN2I1NzdlNDA1NGEiLCJlbWFpbCI6ImtldmlubGluOTYxMUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZTYxNDNmZjY5OWZlN2YwNzcxMWIiLCJzY29wZWRLZXlTZWNyZXQiOiI3YmFjYjg4MzQ4ZmI4MjI4NzE2NTFiMTI0ZjlhYTE2ZDNmYmQ5OTY1MDcyZjUxN2NlZGIyYjgwYTA0MWJhNGJkIiwiZXhwIjoxNzgyODY0MDUxfQ.ZvDteCaLlAkB8a_SRc9eL1jfSPx21mik-u4aKz7Eb60"
+PINATA_GATEWAY = "amber-many-termite-502.mypinata.cloud"
+
+def pin_to_ipfs(data):
+    """
+		Pin to IPFS
+    """
+    assert isinstance(data, dict), "Error pin_to_ipfs expects a dictionary"
+
+    url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {PINATA_JWT}"
+    }
+
+    payload = {
+        "pinataContent": data
+    }
+
+    resp = requests.post(url, headers=headers, json=payload)
+    resp.raise_for_status()
+
+    result = resp.json()
+    # Pinata will return IpfsHash
+    cid = result.get("IpfsHash")
+    return cid
+
+def get_from_ipfs(cid, content_type="json"):
+    """
+    Pull by CID
+    """
+    assert isinstance(cid, str), "get_from_ipfs accepts a cid in the form of a string"
+
+    url = f"https://{PINATA_GATEWAY}/ipfs/{cid}"
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    if content_type.lower() == "json":
+        data = resp.json()
+        assert isinstance(data, dict), "get_from_ipfs dict"
+    else:
+        data = resp.text
+
+    return data
